93dcdeeff9e5853ddfac560672c6d87cfd727b32
nawarajwork-cmd <nawaraj.work@gmail.com>
2026-06-10 03:39:31 +0545

    0 solv js

--- app.js
+++ app.js
@@ -1058,7 +1058,7 @@ function renderCurriculumPanelMarkup() {
 
         out.appendChild(box);
     });
-}
+})
 // ====================================================== // ADD SUBJECT // ======================================================
 async function createNewSubjectNode() {
 const subject =
