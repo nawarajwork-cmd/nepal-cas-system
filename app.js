40902b002c7a5414f3fd8be9078820b92eb7bf9f
nawarajwork-cmd <nawaraj.work@gmail.com>
2026-06-09 19:38:58 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("APP JS LOADED");
 const API_BASE = "https://cas-backend-s9ba.onrender.com/api";
 // ====================================================== // SESSION // ======================================================
 let SESSION_TOKEN = sessionStorage.getItem('CAS_ACTIVE_JWT') || null;
